0526bf5800103ef4efe05ee9e81095b177adb047
Calvin <youngexplicitpvp@gmail.com>
2024-07-10 18:16:20 -0500

    Entities

--- src/main/kotlin/example/eden/ModEntities.kt
+++ src/main/kotlin/example/eden/ModEntities.kt
@@ -15,6 +15,6 @@ object ModEntities {
     // the returned ObjectHolderDelegate can be used as a property delegate
     // this is automatically registered by the deferred registry at the correct times
     val CUSTOM_FIREBALL by ENTITY_REGISTRY.registerObject("custom_fireball") {
-        EntityType.Builder.of(::Meteor, MobCategory.MISC).sized(3.0F, 0.5F).clientTrackingRange(100).setUpdateInterval(1).build("custom_fireball")
+        EntityType.Builder.of(::Meteor, MobCategory.MISC).sized(3.0F, 0.5F).clientTrackingRange(200).setTrackingRange(200).setUpdateInterval(1).build("custom_fireball")
     }
 }
